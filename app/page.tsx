0d50d019a09fcc6b42d72afcafcc74e937d896de
RohmaButt <rohma.butt@gmail.com>
2024-01-10 02:42:35 +0500

    header+footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,34 @@ import Image from "next/image";
 import { Component } from "react";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <>
+      <div className="hero">
+        <div className="container">
+          <h1>Welcome to our website</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            repudiandae.
+          </p>
+        </div>
+      </div>
+      <section className="container content">
+        <h2>content 1</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, animi
+          sunt! Libero optio, asperiores quis ipsa eum eveniet labore nihil
+          expedita nostrum laborum dicta at deleniti sapiente atque assumenda
+          quo corrupti exercitationem animi minus nemo vero soluta alias
+          perspiciatis! Aspernatur, laborum consectetur repellat repellendus
+          aliquam sed. Cupiditate accusantium porro at quae, maxime itaque
+          accusamus! Ipsam tempore, illo similique officia neque asperiores
+          eveniet omnis rem veritatis cumque quos amet placeat dignissimos
+          quisquam nesciunt quas. Adipisci perspiciatis veniam, voluptatibus
+          dolores doloremque quisquam qui possimus, vero ab dolorum temporibus
+          soluta libero similique assumenda debitis dolorem ut perferendis
+          incidunt nobis facere eaque. Enim, nemo?
+        </p>
+      </section>
+    </>
+  );
 }
