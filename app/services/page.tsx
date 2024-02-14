1c37ebfc9ac26e76b08ef318d5e43e2864a27468
RohmaButt <rohma.butt@gmail.com>
2024-02-15 00:12:45 +0500

    services route fixed

---
+++ app/services/page.tsx
@@ -0,0 +1,10 @@
+// "use client";
+import HomePage from "@/shared/components/HomePage";
+import MainWrapper from "@/shared/components/MainWrapper";
+import { Component } from "react";
+
+const Services = () => {
+  return <HomePage />;
+};
+
+export default MainWrapper(Services);
